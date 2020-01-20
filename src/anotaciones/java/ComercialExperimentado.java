f3b5d41849d8e074cbaab4269025f12a4f69b154
paitorre123 <paitorre@alumnos.ubiobio.cl>
2020-01-20 13:03:34 -0300

    uso de @autowired sobre metodos

--- src/anotaciones/java/ComercialExperimentado.java
+++ src/anotaciones/java/ComercialExperimentado.java
@@ -10,11 +10,9 @@ public class ComercialExperimentado extends Empleado{
 	
 	private ICreacionInformeFinanciero informeFinanciero;
 	
-	@Autowired
-	public ComercialExperimentado(ICreacionInformeFinanciero informeFinanciero) {
+	
+	public ComercialExperimentado() {
 		super();
-		this.informeFinanciero = informeFinanciero;
-		
 	}
 
 	@Override
@@ -25,6 +23,11 @@ public class ComercialExperimentado extends Empleado{
 	@Override
 	public String getInforme() {
 		return "Comercial experimentado: " + this.informeFinanciero.getInformeFinanciero();
+	}
+	
+	@Autowired
+	public void setInformeFinanciero(ICreacionInformeFinanciero informeFinanciero) {
+		this.informeFinanciero = informeFinanciero;
 	}
 
 	
